8d360db30ccadace6f2ab499228547df45f1d218
zpop <zpop@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-24 22:37:43 +0000

    First commit
    git-svn-id: https://plugins.svn.wordpress.org/ubuntu-sidebar-lite/trunk@563316 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,49 @@
+=== Ubuntu sidebar lite ===
+Contributors: zpop
+Donate link: http://zeljko.popivoda.com/donacije
+Tags: ubuntu, share, spread, spreadubuntu, sidebar
+Requires at least: 2.0.2
+Tested up to: 3.4
+Stable tag: 0.1
+License: GPLv2
+
+A Ubuntu menu for your website shows Ubuntu networking icons in a un-obtrusive way.
+
+== Description ==
+
+Ubuntu sidebar lite is made to promote Ubuntu.
+
+This plugin is lite version of [Ubuntu sidebar](http://wordpress.org/extend/plugins/ubuntu-sidebar/) without javascript just php, xhtml and css.
+
+Plugin add Ubuntu media buttons to the right side of your website with style and ease.
+
+How Ubuntu sidebar lite looks see at [ZPop](http://zpop.site90.net/blog/ubuntu-sidebar-lite) blog.
+
+On serbian at [Zeljko Popivoda](http://zeljko.popivoda.com/2012/06/ubuntu-sidebar-lite-wordpress-plugin) blog plugin page.
+
+My other plugins:
+* [Ubuntu sidebar](http://wordpress.org/extend/plugins/ubuntu-sidebar/)
+* [Ubuntu ribbon](http://wordpress.org/extend/plugins/ubuntu-ribbon/)
+
+== Installation ==
+
+1. Upload folder `ubuntu-sidebar-lite` in Wordpress plugin directory `/wp-content/plugins/`.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. Your done!
+
+== Frequently Asked Questions ==
+
+If you have any questions, ask them on [Zeljko Popivoda](http://zeljko.popivoda.com/2012/06/ubuntu-sidebar-lite-wordpress-plugin) blog plugin page.
+
+== Screenshots ==
+
+1. What the plugin looks like once installed on your blog or website.
+
+== Changelog ==
+
+= 0.1 =
+* First version.
+
+== Upgrade Notice ==
+
+
---
+++ ubuntu-sidebar-lite.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * @package ubuntu-sidebar-lite
+ * @version 0.1
+ */
+/*
+Plugin Name: Ubuntu sidebar lite
+Plugin URI: http://wordpress.org/extend/plugins/ubuntu-sidebar-lite
+Description: When activated, this plugin will put a Ubuntu sidebar lite on the right side of your website.
+Author: Zeljko Popivoda
+Version: 0.1
+License: GPLv2
+Author URI: http://zeljko.popivoda.com
+*/
+
+function render_ubuntu_sidebar_lite() {
+	$ubuntucom_url = plugins_url('images/ubuntu-com.png', __FILE__ );
+	$ubuntuloco_url = plugins_url('images/ubuntu-loco.png', __FILE__ );
+	$whyubuntu_url = plugins_url('images/why-ubuntu.png', __FILE__ );
+	$ubuntutour_url = plugins_url('images/ubuntu-tour.png', __FILE__ );
+	$downloadubuntu_url = plugins_url('images/download-ubuntu.png', __FILE__ );
+	$ubuntudvd_url = plugins_url('images/ubuntu-dvd.png', __FILE__ );	
+	if(function_exists('is_admin_bar_showing')) {
+		$padding_top = is_admin_bar_showing() ? 148 : 120;
+	} else {
+		$padding_top = 120;
+	}
+	echo "
+		
+	<div id='ubuntuSidebarLite' style='position:fixed; top:".$padding_top."px; right:-3px; z-index:10000; cursor: pointer; border:none;'>
+	
+	<div class='ubuntuNetwork'>
+	<a target='_blank' title='Ubuntu' href='http://www.ubuntu.com/'>
+	<img style='z-index: 10000;' src='{$ubuntucom_url}' /></a></div>
+	
+	<div class='ubuntuNetwork'>
+	<a target='_blank' title='Ubuntu LoCo' href='http://loco.ubuntu.com/'>
+	<img style='z-index: 10000;' src='{$ubuntuloco_url}' /></a></div>
+	
+	<div class='ubuntuNetwork'>
+	<a target='_blank' title='Why use Ubuntu' href='http://www.ubuntu.com/ubuntu/why-use-ubuntu'>
+	<img style='z-index: 10000;' src='{$whyubuntu_url}' /></a></div>
+	
+	<div class='ubuntuNetwork'>
+	<a target='_blank' title='Ubuntu online tour' href='http://www.ubuntu.com/tour'>
+	<img style='z-index: 10000;' src='{$ubuntutour_url}' /></a></div>
+	
+	<div class='ubuntuNetwork'>
+	<a target='_blank' title='Download Ubuntu' href='http://www.ubuntu.com/download'>
+	<img style='z-index: 10000;' src='{$downloadubuntu_url}' /></a></div>
+	
+	<div class='ubuntuNetwork'>
+	<a target='_blank' title='Get Ubuntu CD/DVD' href='http://shop.canonical.com/index.php?cPath=17'>
+	<img style='z-index: 10000;' src='{$ubuntudvd_url}' /></a></div>
+	
+	</div>
+	
+	<style>
+	.ubuntuNetwork:hover {
+	margin-left:-3px;
+	}
+	</style>
+	
+	";
+}
+add_action('wp_footer', 'render_ubuntu_sidebar_lite');
